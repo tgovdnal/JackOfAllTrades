a3a927bb4e942c6a53f18e2155dad9968ea0769c
Readme <project_5695_bot_f9e9705124c63de327d90e291f247feb@noreply.git.acadgraph.de>
2024-10-13 11:42:39 +0200

    adds caching

--- JackOfAllTrades.AppHost/Program.cs
+++ JackOfAllTrades.AppHost/Program.cs
@@ -1,12 +1,12 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-//var cache = builder.AddRedis("cache");
+var cache = builder.AddRedis("cache");
 
 var apiService = builder.AddProject<Projects.JackOfAllTrades_ApiService>("apiservice");
 
-builder.AddProject<Projects.JackOfAllTrades_Web>("webfrontend")
+builder.AddProject<Projects.JackOfAllTrades_Web>("web")
     .WithExternalHttpEndpoints()
-    //.WithReference(cache)
+    .WithReference(cache)
     .WithReference(apiService);
 
 builder.Build().Run();
